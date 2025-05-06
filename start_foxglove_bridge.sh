70d02f64c3cf0eb3352bfe753f2d0161208f8572
Brian Erickson <berickson@gmail.com>
2025-05-06 07:14:55 -0700

    make bridge work on other computers

--- start_foxglove_bridge.sh
+++ start_foxglove_bridge.sh
@@ -5,7 +5,7 @@ echo "Sourcing ROS Humble..."
 source /opt/ros/humble/setup.bash || { echo "Failed to source ROS Humble"; exit 1; }
 
 echo "Building workspace..."
-cd ~/Projects/wc_ws || { echo "Failed to enter workspace directory"; exit 1; }
+cd ./ws || { echo "Failed to enter workspace directory"; exit 1; }
 colcon build || { echo "Failed to build workspace"; exit 1; }
 
 echo "Sourcing workspace..."
